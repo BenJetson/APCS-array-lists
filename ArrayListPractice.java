bc6141cc1116121197b17aa02afcd4c366c7ae31
Ben Godfrey <BenJetson@users.noreply.github.com>
2018-01-29 10:26:05 -0500

    added predictions

--- ArrayListPractice.java
+++ ArrayListPractice.java
@@ -15,7 +15,11 @@ public class ArrayListPractice
 		
 		// #0
 		System.out.println(letters.size() + " " + letters.isEmpty());
-								
+		/**
+		 * Should print
+		 * 0 true
+		 */		
+		
 		// #1
 		letters.add("a");
 		letters.add("b");
@@ -24,31 +28,54 @@ public class ArrayListPractice
 
 		System.out.println(letters.size() + " " + letters.isEmpty());
 
+		/**
+		 * Should print 
+		 * 4 false
+		 */
+
 		// #2 
 		System.out.println(letters);
 		System.out.println(letters.get(3));
+
+		/**
+		 * Should print
+		 * [a, b, c, d]
+		 * d
+		 */
 
 		// #3
 		letters.add("e");
 		letters.add(2,"x");
 		System.out.println(letters);
-				
+		/**
+		 * [a, b, x, c, d, e]
+		 */
+		
 		// #4
 		letters.set(0,"z");
 		letters.remove("c");	
 		letters.remove(3);
 		System.out.println(letters);
-		
+		/**
+		 * [z, b, x, e]
+		 */
+
 		// #5
 		for (int i=letters.size()-1; i>=0; i--)
 			System.out.print(letters.get(i));
 		System.out.println();	
-		
+		/**
+		 * exbz
+		 */
+
 		// #6
 		for (String let: letters)
 			System.out.print(let);
 		System.out.println();
-			
+		/** 
+		 * zbxe
+		 */	
+
 		// #7
 		ArrayList<String> more = new ArrayList<String>();
 		more.add("q");
@@ -59,5 +86,8 @@ public class ArrayListPractice
 			letters.add(0, let);
 			
 		System.out.println(letters);
+		/**
+		 * [q, r, s, z, b, x, e]
+		 */
 	}
 }
